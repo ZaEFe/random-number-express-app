c3e56f78a1adb4adcbc07ca92daf65e0ba0ff000
Zachary <zef20@email.vccs.edu>
2025-03-13 00:15:20 -0400

    Created initial app/server in tutorial and added Comments for code sections

---
+++ server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+
+/* ## VARIABLES ## */
+const app = express();
+const port = 3003;
+const N_NUMBERS= 17;
+
+/* ## MIDDLEWARE (Locations allowed to talk to server/"us") ## */
+const origins = [/https?:\/\/localhost(:\d+)?/, "https://www.google.com", /https:\/\/([a-zA-Z0-9-]+\.)*vccs\.edu/,];
+const corsOpts = {
+    origin: origins,
+};
+
+app.use(cors(corsOpts));
+
+/*## FUNCTIONS ##*/
+function getRandom() {
+    //rand from 0 to 100
+    return Math.floor(Math.random() * 101);
+}
+
+/* #LearningNote# This returns JSON data NOT HTML! */
+app.get('/', (req, res) => {
+    res.json(Array.from({length: N_NUMBERS}, getRandom));
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
