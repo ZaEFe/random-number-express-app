d3f71d106588ba2bc0572fe1966cfc97746a8fc5
Zachary <zef20@email.vccs.edu>
2025-03-14 21:24:28 -0400

    attempting to update CORS origins to see if it works

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
 # Node Express API
 
-This is a simple Node.js API built with Express.
+This is a simple Dockerized Node.js local-fetching API built with Express.
 
 ## Prerequisites
 
+- Docker Desktop
 - Node.js (v14 or higher)
 - npm (v6 or higher)
 
 ## Installation (CLI)
 
 1. Clone the repository:
-    ```sh
-    git clone https://github.com/yourusername/node-express-api.git
-    ```
+   ```sh
+   git clone https://github.com/yourusername/node-express-api.git
+   ```
 2. Navigate to the project directory:
-    ```sh
-    cd node-express-api
-    ```
+   ```sh
+   cd node-express-api
+   ```
 3. Install the dependencies:
-    ```sh
-    npm install
-    ```
+   ```sh
+   npm install
+   ```
 
 ## Building the API
 
@@ -33,10 +34,15 @@ To build the API, use the following command:
 ## Running the API
 
 To run the API, use the following command:
+
 ```sh
 ./scripts/run.sh
 ```
 
 ## Once Operational
+
+Go to localhost:3003 and refresh the page to see the numbers.
+
+## Extra Functionality (Still Testing)
 
 After getting the API running, clicking the button on the local-fetch page that linked you here will return the randomly generated numbers from this API.
--- server.js
+++ server.js
@@ -1,30 +1,36 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 /* ## VARIABLES ## */
 const app = express();
 const port = 3003;
-const N_NUMBERS= 17;
+const N_NUMBERS = 17;
 
 /* ## MIDDLEWARE (Locations allowed to talk to server/"us") ## */
-const origins = [/https?:\/\/localhost(:\d+)?/, "https://www.google.com", /https:\/\/([a-zA-Z0-9-]+\.)*vccs\.edu/,];
+const origins = [
+  /https?:\/\/localhost(:\d+)?/,
+  "https://www.google.com",
+  /https:\/\/([a-zA-Z0-9-]+\.)*vccs\.edu/,
+  /https:\/\/zaefe\.github\.io/,
+  "https://zaefe.github.io/client-side-demos/demos-projects/local_fetch/",
+];
 const corsOpts = {
-    origin: origins,
+  origin: origins,
 };
 
 app.use(cors(corsOpts));
 
 /*## FUNCTIONS ##*/
 function getRandom() {
-    //rand from 0 to 100
-    return Math.floor(Math.random() * 101);
+  //rand from 0 to 100
+  return Math.floor(Math.random() * 101);
 }
 
 /* #LearningNote# This returns JSON data NOT HTML! */
-app.get('/', (req, res) => {
-    res.json(Array.from({length: N_NUMBERS}, getRandom));
+app.get("/", (req, res) => {
+  res.json(Array.from({ length: N_NUMBERS }, getRandom));
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
